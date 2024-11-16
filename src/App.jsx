caf70836d552a2b515024c3a76405679a7c7bf5b
Alexandre Cruv <alefeufei@gmail.com>
2024-11-16 11:08:56 -0300

    Componente banner - parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <Header/>
+    <Banner/>
       <h1>Hello World!</h1>
       <p>Olá mundo</p>
       <Footer/>
