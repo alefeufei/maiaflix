7131117b3c926b4de95736920cdf78a30398e254
Alexandre Cruv <alefeufei@gmail.com>
2024-11-16 01:49:20 -0300

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+      <h1>Hello World!</h1>
+      <p>Olá mundo</p>
+      <Footer/>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import styles from "./Footer.module.css"
+
+function Footer()  {
+
+return (
+    <footer className={styles.footer}>
+
+        <h2>Maia Flix &copy; Desenvolvido por Alexandre Cruv 2024</h2>
+    </footer>
+)
+
+}
+
+export default Footer
